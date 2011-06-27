41ca9d730644031e88b30832f087c4fb9568276e
aimee <aimee@aimee-VirtualBox.(none)>
2011-06-27 22:29:01 +0100

    A knowledge base of books and authors.

---
+++ week3-prolog/books.pl
@@ -0,0 +1,10 @@
+wrote(harry_potter, j_k_rowling).
+wrote(fantastic_beasts, j_k_rowling).
+wrote(quidditch_through_the_ages, j_k_rowling).
+
+wrote(the_girl_with_the_dragon_tattoo, stieg_larsson).
+wrote(the_girl_who_played_with_fire, stieg_larsson).
+wrote(the_girl_who_kicked_the_hornets_nest, stieg_larsson).
+
+wrote(animal_farm, george_orwell).
+wrote(1984, george_orwell).
